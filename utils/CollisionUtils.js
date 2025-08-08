4ee69921a450e716f551f5f425859f74a15da823
Tvalc <tony@glittercloud.solutions>
2025-08-08 07:44:57 -0700

    Update from Snib Studio - 2025-08-08T14:44:27.794Z

---
+++ utils/CollisionUtils.js
@@ -0,0 +1,12 @@
+// CollisionUtils: rectsOverlap, etc.
+const CollisionUtils = {
+  rectsOverlap(a, b) {
+    return (
+      a.x < b.x + b.w &&
+      a.x + a.w > b.x &&
+      a.y < b.y + b.h &&
+      a.y + a.h > b.y
+    );
+  }
+};
+window.CollisionUtils = CollisionUtils;
