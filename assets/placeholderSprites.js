f7586c0dbe981d9187fa7da1cfe922ed961695b4
Tvalc <tony@glittercloud.solutions>
2025-08-08 07:44:56 -0700

    Update from Snib Studio - 2025-08-08T14:44:27.794Z

---
+++ assets/placeholderSprites.js
@@ -0,0 +1,8 @@
+// PlaceholderSprites: code-generated SVG/Canvas data for player/enemy/etc.
+const PlaceholderSprites = {
+  player: null, // Not used, drawn in code
+  enemy: null,
+  prop: null,
+  powerup: null
+};
+window.PlaceholderSprites = PlaceholderSprites;
